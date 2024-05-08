5db8ce2843f4cddae7df3084b54cc94e3591271b
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-08 22:58:00 +0700

    update: Tipe Data Bukan Primitif

---
+++ src/TipeDataBukanPrimitif.java
@@ -0,0 +1,21 @@
+public class TipeDataBukanPrimitif {
+    public static void main(String[] args) {
+        // Representasi Tipe Data Primitif (1)
+        // Primitif -> Bukan Primitif
+        // byte -> Byte
+        // short -> Short
+        // int -> Integer
+        // long -> Long
+        // float -> Float
+        // double -> Double
+        // char -> Character
+        // boolean -> Boolean
+
+        //Example
+        Integer iniInteger = 10;
+        Long iniLong = 10L;
+        Boolean iniBoolean = true;
+        Short iniShort; // null
+        iniShort = 100;
+    }
+}
