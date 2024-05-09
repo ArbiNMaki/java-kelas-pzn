f2efb3f8cdc2ab0142911e6f05ad12a3e45f2828
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-09 22:39:01 +0700

    update: percabangan IF

---
+++ src/Percabangan/PercabanganIF.java
@@ -0,0 +1,12 @@
+package Percabangan;
+
+public class PercabanganIF {
+    public static void main(String[] args) {
+        var nilai = 75;
+        var absen = 85;
+
+        if(nilai >= 75 && absen >= 75) {
+            System.out.println("Anda Lulus.");
+        }
+    }
+}
