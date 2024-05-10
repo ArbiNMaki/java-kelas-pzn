8ddf7293677e7891ffb6d5d60fb1893a974c2db3
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-10 08:43:04 +0700

    update: Method

---
+++ src/Method.java
@@ -0,0 +1,14 @@
+public class Method {
+    public static void main(String[] args) {
+        sayHelloWorld();
+        bilangHaloDunia();
+    }
+
+    static void sayHelloWorld() {
+        System.out.println("Halo Dunia.");
+    }
+
+    static void bilangHaloDunia() {
+        System.out.println("Hello World.");
+    }
+}
