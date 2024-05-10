818f52734904de821acff204a8b9cd915263ffaf
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-10 10:25:38 +0700

    update: Method Overloading

--- src/MethodJava/MethodOverloading.java
+++ src/MethodJava/MethodOverloading.java
@@ -2,6 +2,20 @@ package MethodJava;
 
 public class MethodOverloading {
     public static void main(String[] args) {
+        sayHello();
+        sayHello("Bakugou");
+        sayHello("Bakugou", "Katsuki");
+    }
 
+    static void sayHello() {
+        System.out.println("Hello");
+    }
+
+    static void sayHello(String firstName) {
+        System.out.println("Hello " + firstName);
+    }
+
+    static void sayHello(String firstName, String lastName) {
+        System.out.println("Hello " + firstName + " " + lastName);
     }
 }
