116faa2e835d85694ac256d7cb381573240aa392
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-10 07:58:51 +0700

    update: Do-While Loop

--- src/Perulangan/PerulanganDoWhile.java
+++ src/Perulangan/PerulanganDoWhile.java
@@ -1,4 +1,19 @@
 package Perulangan;
 
 public class PerulanganDoWhile {
+    public static void main(String[] args) {
+        // Syntax Do-While Loop
+        /*
+            do {
+                // Block Perulangan
+            } while (condition;)
+        */
+
+        var counter = 100;
+
+        do {
+            System.out.println("Perulangan ke-" + counter);
+            counter++;
+        } while (counter <= 20);
+    }
 }
