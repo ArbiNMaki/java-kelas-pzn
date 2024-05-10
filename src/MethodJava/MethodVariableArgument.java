b6673006b3692acc95ee57388835dd8259393aa9
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-10 10:18:28 +0700

    update: Method Variable Argument

--- src/MethodJava/MethodVariableArgument.java
+++ src/MethodJava/MethodVariableArgument.java
@@ -2,6 +2,41 @@ package MethodJava;
 
 public class MethodVariableArgument {
     public static void main(String[] args) {
+        int[] values = {
+                75, 77, 87, 95, 100
+        };
+        sayCongrats("Arbi", values);
+        sayCongrat("Budi", 90, 75, 26, 65, 70);
+    }
 
+    // Tanpa Variable Argument
+   static void sayCongrats(String name, int[] values) {
+       var total = 0;
+       for(var value : values) {
+           total += value;
+       }
+       int finalValue = total / values.length;
+
+       if(finalValue >= 70) {
+           System.out.println("Selamat " + name + ", Anda lulus.");
+       } else {
+           System.out.println("Maaf " + name + ", Anda tidak lulus.");
+       }
+   }
+
+
+    // Dengan Variable Argument
+    static void sayCongrat(String name, int... values) {
+        var total = 0;
+        for(var value : values) {
+            total += value;
+        }
+        int finalValue = total / values.length;
+
+        if(finalValue >= 70) {
+            System.out.println("Selamat " + name + ", Anda lulus.");
+        } else {
+            System.out.println("Maaf " + name + ", Anda tidak lulus.");
+        }
     }
 }
