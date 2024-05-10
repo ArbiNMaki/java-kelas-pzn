6fc21a65138ad0842801ac34ee18ebec82b11cd6
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-10 07:51:00 +0700

    update: While Loop

--- src/Perulangan/PerulanganWhile.java
+++ src/Perulangan/PerulanganWhile.java
@@ -1,4 +1,19 @@
 package Perulangan;
 
 public class PerulanganWhile {
+    public static void main(String[] args) {
+        // Syntax While Loop
+        /*
+            while (condition;) {
+                // Block perulangan
+            }
+        */
+
+        var counter = 1;
+
+        while (counter <= 20) {
+            System.out.println("Perulangan ke-" + counter);
+            counter++;
+        }
+    }
 }
