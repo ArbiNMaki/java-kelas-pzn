3cc92de821fa2e0bccd3b90cd0c7a90326494374
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-11 14:29:59 +0700

    update: OOP (Class, Object, Field)

---
+++ src/OOP/PersonApp.java
@@ -0,0 +1,25 @@
+package OOP;
+
+public class PersonApp {
+    public static void main(String[] args) {
+
+        var person = new Person();
+        person.name = "Arbi Dwi Wijaya";
+        person.address = "Malang";
+//        person.country = "Tidak Bisa Diubah";
+
+        System.out.println(person.name);
+        System.out.println(person.address);
+        System.out.println(person.country);
+
+//        var person1 = new Person();
+//        Person person2 = new Person();
+//        Person person3;
+//        person3 = new Person();
+//
+//        System.out.println(person1);
+//        System.out.println(person2);
+//        System.out.println(person3);
+
+    }
+}
