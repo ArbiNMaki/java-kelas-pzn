82f99ef2f78ffb182dfdd1f19c29d98c5be67171
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-11 14:44:51 +0700

    update: OOP (Method)

--- src/OOP/Person.java
+++ src/OOP/Person.java
@@ -4,4 +4,10 @@ class Person {
     String name;
     String address;
     final String country = "INDONESIA";
+
+    void sayHello(String paramName) {
+        System.out.println("Hello " + paramName + ", My Name is " + name);
+        System.out.println("I Live in " + country);
+        System.out.println("My home address is at " + address);
+    }
 }
--- src/OOP/PersonApp.java
+++ src/OOP/PersonApp.java
@@ -12,14 +12,7 @@ public class PersonApp {
         System.out.println(person.address);
         System.out.println(person.country);
 
-//        var person1 = new Person();
-//        Person person2 = new Person();
-//        Person person3;
-//        person3 = new Person();
-//
-//        System.out.println(person1);
-//        System.out.println(person2);
-//        System.out.println(person3);
+        person.sayHello("Katsuki");
 
     }
 }
