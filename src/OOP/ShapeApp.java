617ded03d0b65f2034cbcac1e52cf27241747737
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-11 21:01:57 +0700

    update: OOP (edit super Keyword)

--- src/OOP/ShapeApp.java
+++ src/OOP/ShapeApp.java
@@ -2,6 +2,9 @@ package OOP;
 
 public class ShapeApp {
     public static void main(String[] args) {
+        var kotak = new Shape();
+        System.out.println(kotak.getCorner());
+
         var persegi = new Rectangle();
         System.out.println(persegi.getCorner());
         System.out.println(persegi.getParentCorner());
