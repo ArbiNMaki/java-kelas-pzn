30d058df2fe57bdbb9b256e48e09101c0964074a
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-12 00:15:43 +0700

    updated: OOP (super constructor)

--- src/OOP/Manager.java
+++ src/OOP/Manager.java
@@ -3,12 +3,20 @@ package OOP;
 class Manager {
     String name;
 
+    Manager(String name) {
+        this.name = name;
+    }
+
     void sayHello(String name) {
         System.out.println("Hello " + name + ", My Name is Manager " + this.name);
     }
 }
 
 class ViceManager extends  Manager {
+    ViceManager(String name) {
+        super(name);
+    }
+
     void sayHello(String name) {
         System.out.println("Hello " + name + ", My Name is VM " + this.name);
     }
--- src/OOP/ManagerApp.java
+++ src/OOP/ManagerApp.java
@@ -2,12 +2,12 @@ package OOP;
 
 public class ManagerApp {
     public static void main(String[] args) {
-        var manager = new Manager();
-        manager.name = "Arbi";
+        var manager = new Manager("Arbi");
+//        manager.name = "Arbi";
         manager.sayHello("Katsuki");
 
-        var viceManager = new ViceManager();
-        viceManager.name = "Maki";
+        var viceManager = new ViceManager("Maki");
+//        viceManager.name = "Maki";
         viceManager.sayHello("Katsuki");
     }
 }
