cbcb81b0cc755e3cad22beb3f78a9f13ec37711f
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-12 16:03:45 +0700

    update: OOP(Package)

---
+++ src/OOP/belajarjava/data/Product.java
@@ -0,0 +1,11 @@
+package OOP.belajarjava.data;
+
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
