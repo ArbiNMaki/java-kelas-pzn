ce530818ca645c78527a45a4c2e0a77e0ac4ed9f
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-13 04:02:25 +0700

    update: OOP(toString Method)

--- src/OOP/belajarjava/app/ProductApp.java
+++ src/OOP/belajarjava/app/ProductApp.java
@@ -1,14 +1,19 @@
-package OOP.belajarjava.data;
+package OOP.belajarjava.app;
+
+import OOP.belajarjava.data.Product;
 
 public class ProductApp {
     public static void main(String[] args) {
         Product product = new Product("Mac", 10_000_000);
         System.out.println(product.name);
         System.out.println(product.price);
+
 //        product.name = "Indomie"; // Bisa diakses karena di package sama
 //        product.price = 3500; // Bisa diakses karena di package sama
 //
 //        System.out.println(product.getName());
 //        System.out.println(product.getPrice());
+
+        System.out.println(product);
     }
 }
