2a34a9009c4c8ee91aff6ee61048a5ec1c0e2a16
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-13 17:57:41 +0700

    update: OOP(Final Class)

---
+++ src/OOP/belajarjava/data/SocialMedia.java
@@ -0,0 +1,12 @@
+package OOP.belajarjava.data;
+
+class SocialMedia {
+    String name;
+}
+
+final class Facebook extends SocialMedia {
+
+}
+
+//class FakeFacebook extends Facebook { // Error
+//}
