3668c8d2b3ac85b39835dd91e99224a4ca50d50c
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-13 19:09:47 +0700

    update: OOP(Anonymous Class)

---
+++ src/OOP/belajarjava/anonymous/HelloWorld.java
@@ -0,0 +1,7 @@
+package OOP.belajarjava.anonymous;
+
+public interface HelloWorld {
+    void sayHello();
+
+    void sayHello(String name);
+}
---
+++ src/OOP/belajarjava/app/HelloWorldApp.java
@@ -0,0 +1,20 @@
+package OOP.belajarjava.app;
+
+import OOP.belajarjava.anonymous.HelloWorld;
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        HelloWorld english = new HelloWorld() {
+            public void sayHello() {
+                System.out.println("Hello");
+            }
+
+            public void sayHello(String name) {
+                System.out.println("Hello " + name);
+            }
+        };
+
+        english.sayHello();
+        english.sayHello("Arbi");
+    }
+}
