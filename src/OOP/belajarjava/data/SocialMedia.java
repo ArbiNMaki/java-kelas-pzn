a421122e7928ed2ecefe443f76695b4a4801c380
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-13 18:08:03 +0700

    update: OOP(Final Method)

--- src/OOP/belajarjava/data/SocialMedia.java
+++ src/OOP/belajarjava/data/SocialMedia.java
@@ -10,3 +10,15 @@ final class Facebook extends SocialMedia {
 
 //class FakeFacebook extends Facebook { // Error
 //}
+
+class Twitter extends SocialMedia {
+    final void login(String username, String password) {
+
+    }
+}
+
+//class FakeTwitter extends Twitter {
+//    void login(String username, String password) { // Error
+//    }
+//}
+
