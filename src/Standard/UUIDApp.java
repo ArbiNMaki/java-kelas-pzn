360f6a2821ae68e40e3aa42b42373556b5e3da38
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-15 00:19:54 +0700

    update: Standard(UUID class)

---
+++ src/Standard/UUIDApp.java
@@ -0,0 +1,12 @@
+package Standard;
+
+import java.util.UUID;
+
+public class UUIDApp {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            UUID uuid = UUID.randomUUID();
+            System.out.println(uuid);
+        }
+    }
+}
