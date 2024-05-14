8c0c2a30eeccc99a6b44c333085c72ecd96be218
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-14 20:35:13 +0700

    update: Standard(Math class)

---
+++ src/Standard/MathApp.java
@@ -0,0 +1,11 @@
+package Standard;
+
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(1000, 1001);
+        System.out.println("Terkecil adalah = " + min);
+
+        var max = Math.max(1000, 1001);
+        System.out.println("Terbesar adalah = " + max);
+    }
+}
