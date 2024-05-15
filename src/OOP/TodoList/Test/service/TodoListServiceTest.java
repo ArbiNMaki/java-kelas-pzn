fdba756501cb466ed14acfef3756524ea2b16438
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-15 18:57:02 +0700

    update: OOP TodoList(Test Repository and Service add TodoList)

--- src/OOP/TodoList/Test/service/TodoListServiceTest.java
+++ src/OOP/TodoList/Test/service/TodoListServiceTest.java
@@ -8,16 +8,27 @@ import OOP.TodoList.Service.TodoListServiceImpl;
 
 public class TodoListServiceTest {
     public static void main(String[] args) {
-        testShowTodoList();
+        testAddTodoList();
     }
 
     public static void testShowTodoList() {
         TodoListRepositoryImpl todoListRepository = new TodoListRepositoryImpl();
-        todoListRepository.data[0] = new TodoList("Belajar Java Dasar");
+        todoListRepository.data[0] = new TodoList("Belajar Java Basic");
         todoListRepository.data[1] = new TodoList("Belajar Java Intermediate");
         todoListRepository.data[2] = new TodoList("Belajar Java Advance");
 
         TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+        todoListService.showTodoList();
+    }
+
+    public static void testAddTodoList() {
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+        todoListService.addTodoList("Belajar Java Basic");
+        todoListService.addTodoList("Belajar Java Intermediate");
+        todoListService.addTodoList("Belajar Java Advance");
 
         todoListService.showTodoList();
     }
