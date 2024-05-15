a0829ffa832496c90ba459bddbb0ce13c2dec887
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-15 18:41:13 +0700

    update: OOP TodoList(Repository and Service add todolist)

--- src/OOP/TodoList/Repository/TodoListRepositoryImpl.java
+++ src/OOP/TodoList/Repository/TodoListRepositoryImpl.java
@@ -11,9 +11,41 @@ public class TodoListRepositoryImpl implements TodoListRepository {
         return data;
     }
 
+    public boolean isFull() {
+        var isFull = true;
+        for(int i = 0; i < data.length; i++) {
+            if(data[i] == null) {
+                // Model masih ada yang kosong
+                isFull = false;
+                break;
+            }
+        }
+        return isFull;
+    }
+
+    public void resizeIfFull() {
+        // Jika isFull, kita resize ukuran array 2x lipat
+        if(isFull()) {
+            var temp = data;
+            data = new TodoList[data.length * 2];
+
+            for(int i = 0; i < temp.length; i++) {
+                data[i] = temp[i];
+            }
+        }
+    }
+
     @Override
     public void add(TodoList todolist) {
+        resizeIfFull();
 
+        // Tambahkan ke posisi yang data array nya NULL
+        for(var i = 0; i < data.length; i++) {
+            if(data[i] == null) {
+                data[i] = todolist;
+                break;
+            }
+        }
     }
 
     @Override
