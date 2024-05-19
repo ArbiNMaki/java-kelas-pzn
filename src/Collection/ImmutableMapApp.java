fc4fbfd3e7be63e40406b7a85725d21cb3d5f31c
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-19 18:53:26 +0700

    update: Collection(Immutable Map)

---
+++ src/Collection/ImmutableMapApp.java
@@ -0,0 +1,23 @@
+package Collection;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImmutableMapApp {
+    public static void main(String[] args) {
+        Map<String, String> empty = Collections.emptyMap();
+        Map<String, String> singleton = Collections.singletonMap("name", "Arbi");
+
+        Map<String, String> mutable = new HashMap<>();
+        mutable.put("name", "Arbi");
+        Map<String, String> immutable = Collections.unmodifiableMap(mutable);
+
+        Map<String, String> map = Map.of(
+                "first", "Arbi",
+                "middle", "Dwi",
+                "last", "Wijaya"
+        );
+//        map.put("a", "A"); // Error
+    }
+}
