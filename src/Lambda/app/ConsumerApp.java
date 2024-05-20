5ec76e3a8919314fd6dae5809590663b34552327
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-20 18:10:17 +0700

    update: Lambda(Java Util Function)

---
+++ src/Lambda/app/ConsumerApp.java
@@ -0,0 +1,11 @@
+package Lambda.app;
+
+import java.util.function.Consumer;
+
+public class ConsumerApp {
+    public static void main(String[] args) {
+        Consumer<String> consumer = value -> System.out.println(value);
+
+        consumer.accept("Arbi Dwi Wijaya");
+    }
+}
